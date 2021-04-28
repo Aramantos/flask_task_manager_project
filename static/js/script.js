bd574f83548d4276ebeae158448ce5e89595faea
Aramantos <john.doyle.mail@icloud.com>
2021-04-28 14:10:32 +0000

    add task collapsible setup

--- static/js/script.js
+++ static/js/script.js
@@ -7,4 +7,16 @@
 
   $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
+    $('.collapsible').collapsible();
   });
+
+  /*  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+  }); 
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  }); */
